fabe36667424269e26c492e889194f49f675f8fc
xx1064xx <127997223+xx1064xx@users.noreply.github.com>
2024-02-06 09:17:02 +0100

    innitial

--- src/Program/Controller/Controller.java
+++ src/Program/Controller/Controller.java
@@ -1,9 +1,16 @@
 package Program.Controller;
 
+import Program.DbAccess.DbAccess;
+
 public class Controller {
 
     public void run() {
+
+        DbAccess dbAccess = new DbAccess();
+
         System.out.println("huere mongo 2");
+
+        dbAccess.connectToDb();
     }
 
 }
--- src/Program/DbAccess/DbAccess.java
+++ src/Program/DbAccess/DbAccess.java
@@ -1,4 +1,26 @@
 package Program.DbAccess;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.bson.Document;
+
+
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+
 public class DbAccess {
+
+    private Connection con;
+    private String connectionString = "mongodb://localhost:27017";
+    public void connectToDb() {
+
+        MongoClient mongoClient = MongoClients.create(connectionString);
+
+        List<Document> databases = mongoClient.listDatabases().into(new ArrayList<>());
+
+        System.out.println(databases);
+
+    }
+
 }
